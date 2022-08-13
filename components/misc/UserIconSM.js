b31cdebd856303a2f6733e14542ae5da063a8b75
nabil989 <73457486+nabil989@users.noreply.github.com>
2022-08-13 13:28:32 -0500

    fixed all img tags

--- components/misc/UserIconSM.js
+++ components/misc/UserIconSM.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 export default function UserIconSM ({name, image }) {
     let img = image ? 'data:image/jpeg;base64,' + Buffer.from(image).toString('base64').slice(20) : "";
     return image ? 
-    <img src={img} className= "w-10 h-10 rounded-full" title={name} ></Image>
+    <img src={img} className= "w-10 h-10 rounded-full" title={name} ></img>
     :
     <div className= "w-10 h-10 rounded-full bg-blue-300" title={name}></div>
 }
